56c7e25097083d0a34c992e8f60b5c0607dfa355
scope <scope@scope-X370.(none)>
2014-10-15 17:17:26 -0400

    Refactor random graph to produce dict version
    The dict eases the process of returning a json value

--- graph.py
+++ graph.py
@@ -1,20 +1,16 @@
 import random
 
 class RandomGraph(object):
-    def __init__(self, vertices_num, edges_num):
-        self.vertices_num = vertices_num
-        self.vertices = range(vertices_num)
-        self.edges = self.gen_edges(edges_num)
-        self.random_vertice = random.randrange(0, self.vertices_num)
-        
-    def gen_edges(self, edges_num):
-        x = map(lambda x: random.randrange(0, self.vertices_num), xrange(10))
-        y = map(lambda x: random.randrange(0, self.vertices_num), xrange(10))
-        edges = []
-        for i,j in zip(x,y):
-            edges.append((i,j))
-        return edges
+    def __init__(self, vertices_num):
+        self.vertices = xrange(vertices_num)
+        self.graph = dict(enumerate(self.edges(vertices_num)))
 
-
-z = RandomGraph(4,5)
-j = [random.randrange(0, 10) for x in xrange(10)]
+    def edges(self, vert_num):
+        # Maps each possible vertice to a list of possible adjacent vertices.
+        # The lambda ensures that the vertice being mapped isn't included as a
+        # possible adjacent vertice
+        verts = map(lambda x: (y for y in xrange(vert_num) if x != y), xrange(vert_num))
+        for edges in verts:
+            # Yields a list of vertices connected to single vertex.  The lambda
+            # function filters the possible vertices randomly.
+            yield filter(lambda x: random.choice([True, False]), edges)
