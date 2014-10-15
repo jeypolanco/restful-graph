9a7c18e02849c82cebe3349cd3c61d8173e5f0f6
scope <scope@scope-X370.(none)>
2014-10-14 20:41:30 -0400

    Add more functions to todo web service

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
 #!flask/bin/python
 
-from flask import Flask
+from flask import Flask, jsonify, make_response, abort, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, WOrld!"
+tasks = [
+    {
+        'id': 1,
+        'title': u'buy groceries',
+        'description': u'milk, cheese, pizza, fruit, tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'learn python',
+        'description': u'need to find a good python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+        
+        }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
